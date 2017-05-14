2005592c526bcd3e7fb2faaf77044d75283a4c72
eddymens <kwasiamantin@gmail.com>
2017-05-14 06:06:51 +0000

    add comments to example

--- brainFuck.php
+++ brainFuck.php
@@ -2,51 +2,48 @@
 
 class brainFuck
 {
-  public $argsStack = [];
-  public $memoryStack = [];
-  public $memoryPointer = 0;
-  public $executionStack = [];
-  public $execPrimitive = false;
-  
-  public function __construct()
-  {
-    $this->memoryStack = array_fill(0,30000, null);
-  }
+    public $argsStack = [];
+    public $memoryStack = [];
+    public $memoryPointer = 0;
+    public $executionStack = [];
+    public $execPrimitive = false;
 
-  public function __destruct()
-  {
-    $this->executor(0, count($this->executionStack));
+    public function __construct()
+    {
+        $this->memoryStack = array_fill(0, 30000, null);
+    }
 
-  }
-  
-   /**
-   * walk through and excute stack 
+    public function __destruct()
+    {
+        $this->executor(0, count($this->executionStack));
+    }
+
+  /**
+   * walk through and excute stack.
    *
    * @param string $start
    * @param string $end
    */
   public function executor($start, $end)
   {
-    $this->execPrimitive = true;
-    $count = $start;
-    $args = $this->argsStack;
+      $this->execPrimitive = true;
+      $count = $start;
+      $args = $this->argsStack;
 
-    for($count; $count < $end; $count++){
-      $aPrimitive = $this->executionStack[$count];
+      for ($count; $count < $end; ++$count) {
+          $aPrimitive = $this->executionStack[$count];
 
-      if($aPrimitive == 'label'){
-        $start = $count+1;
-        $label = $args[$count][0];
-        $this->loop($label, $start);
-      } else{
-        call_user_func_array([$this, $aPrimitive], $args[$count]);    
+          if ($aPrimitive == 'label') {
+              $start = $count + 1;
+              $label = $args[$count][0];
+              $this->loop($label, $start);
+          } else {
+              call_user_func_array([$this, $aPrimitive], $args[$count]);
+          }
       }
-
-    }
-    
   }
-  
-   /**
+
+  /**
    * loop over specific sections of the execution stack.
    *
    * @param string $label
@@ -54,168 +51,177 @@ class brainFuck
    */
   public function loop($label, $start)
   {
-   $args = $this->argsStack;
-   $end  = $this->findJmp($label, $start);
+      $args = $this->argsStack;
+      $end = $this->findJmp($label, $start);
 
-   while($this->memoryStack[$this->memoryPointer] > 1){
-     $this->executor($start, $end);
-   }
- }
-
+      while ($this->memoryStack[$this->memoryPointer] > 1) {
+          $this->executor($start, $end);
+      }
+  }
 
  /**
-   * find the jmp statement for a label
-   *
-   * @param string $label
-   * @param int $start
-   */
+  * find the jmp statement for a label.
+  *
+  * @param string $label
+  * @param int $start
+  */
  public function findJmp($label, $start)
  {
-  $args = $this->argsStack;
+     $args = $this->argsStack;
 
-  foreach($this->executionStack as $index => $aPrimitive){
+     foreach ($this->executionStack as $index => $aPrimitive) {
+         if ($aPrimitive == 'jmp') {
+             $end = ($args[$index][0] == $label) ? $index : $start;
+             break;
+         }
+     }
 
-   if($aPrimitive == 'jmp'){
-     $end = ($args[$index][0] == $label)? $index : $start;
-     break;
-   }
-
+     return $end;
  }
 
- return $end;
-}
-
 /**
-   * dump memory stack
-   *
-   * @return string
-   */
+ * dump memory stack.
+ *
+ * @return string
+ */
 private function dumpMemory()
 {
-  echo implode(' ',$this->memoryStack);
+    echo implode(' ', $this->memoryStack);
 }
 
 /**
-   * push metthod sequence to a stack 
-   *
-   * @param string $method
-   * @param array  $args
-   */
+ * push metthod sequence to a stack.
+ *
+ * @param string $method
+ * @param array  $args
+ */
 public function addToExecutionStack($method, $args)
 {
-
- array_push($this->executionStack, $method);
- array_push($this->argsStack, $args);
+    array_push($this->executionStack, $method);
+    array_push($this->argsStack, $args);
 }
 
+    public function __call($method, $args)
+    {
+        if (!$this->execPrimitive) {
+            $this->addToExecutionStack($method, $args);
 
-public function __call($method, $args)
-{
-  if(!$this->execPrimitive){
-    $this->addToExecutionStack($method, $args); 
-    return $this;
-  } else {
-   call_user_func_array([$this, $method], $args);
- }
- return $this;
-}
+            return $this;
+        } else {
+            call_user_func_array([$this, $method], $args);
+        }
+
+        return $this;
+    }
 
 /**
-   * take character and convert to ASCII
-   *
-   * @param string $method
-   * @return isntance
-   */
+ * take character and convert to ASCII.
+ *
+ * @param string $method
+ *
+ * @return isntance
+ */
 private function input($inputValue)
 {
+    $this->memoryStack[$this->memoryPointer] = ord($inputValue);
 
-  $this->memoryStack[$this->memoryPointer] = ord($inputValue);
-  return $this;
-}
-
- /**
-   * take ASCII and convert to character
-   *
-   * @param bol $print
-   * @return isntance
-   */
-private  function  output($print=true)
-{
-  $output = chr($this->memoryStack[$this->memoryPointer]);
-  if($print){echo $output;}
-  return $this;
-}
-
- /**
-   * increment memory counter
-   *
-   * @param string $label
-   * @return instance
-   */
-private function shiftFwd($num=1)
-{
-  $this->memoryPointer += $num;
-  return $this;
+    return $this;
 }
 
 /**
-   * decrease memory counter
-   *
-   * @param string $num
-   * @return instance
-   */
-private function shiftBck ($num=1)
+ * take ASCII and convert to character.
+ *
+ * @param bol $print
+ *
+ * @return isntance
+ */
+private function output($print = true)
 {
-  $this->memoryPointer -= $num;
-  return $this;
+    $output = chr($this->memoryStack[$this->memoryPointer]);
+    if ($print) {
+        echo $output;
+    }
+
+    return $this;
 }
 
 /**
-   * increment cell value
-   *
-   * @param string $num
-   * @return instance
-   */
-private function inc($num=1)
+ * increment memory counter.
+ *
+ * @param string $label
+ *
+ * @return instance
+ */
+private function shiftFwd($num = 1)
 {
-  $this->memoryStack[$this->memoryPointer] += $num; 
-  return $this;
+    $this->memoryPointer += $num;
+
+    return $this;
 }
 
 /**
-   * decrement cell value
-   *
-   * @param string $num
-   * @return instance
-   */
-private function dec($num=1)
+ * decrease memory counter.
+ *
+ * @param string $num
+ *
+ * @return instance
+ */
+private function shiftBck($num = 1)
 {
-  $this->memoryStack[$this->memoryPointer] -= $num;
-  return $this;
+    $this->memoryPointer -= $num;
+
+    return $this;
 }
 
 /**
-   * mark beginning of loop
-   *
-   * @param string $label
-   * @return instance
-   */
+ * increment cell value.
+ *
+ * @param string $num
+ *
+ * @return instance
+ */
+private function inc($num = 1)
+{
+    $this->memoryStack[$this->memoryPointer] += $num;
+
+    return $this;
+}
+
+/**
+ * decrement cell value.
+ *
+ * @param string $num
+ *
+ * @return instance
+ */
+private function dec($num = 1)
+{
+    $this->memoryStack[$this->memoryPointer] -= $num;
+
+    return $this;
+}
+
+/**
+ * mark beginning of loop.
+ *
+ * @param string $label
+ *
+ * @return instance
+ */
 private function label($label)
 {
-  return $this;
+    return $this;
 }
 
 /**
-   * mark end of loop
-   *
-   * @param string $label
-   * @return instance
-   */
-private function jmp($label) 
+ * mark end of loop.
+ *
+ * @param string $label
+ *
+ * @return instance
+ */
+private function jmp($label)
 {
-  return $this;
+    return $this;
 }
 }
-
-
-
--- index.php
+++ index.php
@@ -1,33 +1,32 @@
-<?php 
-include('brainFuck.php');
+<?php
+
+include 'brainFuck.php';
 
 $brainFuck = new brainFuck();
 
 $brainFuck
-    ->inc(10)
+    ->inc(10) //initialize counter (cell #0) to 10
 
-    ->label('blckA')
-        ->shiftFwd()->inc(7)
-        ->shiftFwd()->inc(10)
-        ->shiftFwd()->inc(3)
-        ->shiftFwd()->inc(1)
-        ->shiftBck(4)->dec()
+    ->label('blckA') //use loop to set 70/100/30/10
+        ->shiftFwd()->inc(7) //add  7 to cell #1
+        ->shiftFwd()->inc(10) //add 10 to cell #2
+        ->shiftFwd()->inc(3) //add  3 to cell #3
+        ->shiftFwd()->inc(1) //add  1 to cell #4
+        ->shiftBck(4)->dec() //decrement counter (cell #0)
     ->jmp('blckA')
 
-    ->shiftFwd()->inc(2)->output()
-    ->shiftFwd()->inc(1)->output()
-    ->inc(7)->output()
-    ->output()
-    ->inc(3)->output()
-    ->shiftFwd()->inc(2)->output()
-    ->shiftBck(2)->inc(15)->output()
-    ->shiftFwd()->output()
-    ->inc(3)->output()
-    ->dec(6)->output()
-    ->dec(8)->output()
-    ->shiftFwd()->inc()->output()
-    ->shiftFwd()->output()
-    
+    ->shiftFwd()->inc(2)->output()     //print 'H'
+    ->shiftFwd()->inc(1)->output()     //print 'e'
+    ->inc(7)->output()                 //print 'l'
+    ->output()                         //print 'l'
+    ->inc(3)->output()                 //print 'o'
+    ->shiftFwd()->inc(2)->output()     //print ' '
+    ->shiftBck(2)->inc(15)->output()   //print 'W'
+    ->shiftFwd()->output()			   //print 'o'
+    ->inc(3)->output()                 //print 'r'
+    ->dec(6)->output()				   //print 'l'
+    ->dec(8)->output()				   //print 'd'
+    ->shiftFwd()->inc()->output()	   //print '!'
+    ->shiftFwd()->output()             //print '\n'
+
 ;
-
-
